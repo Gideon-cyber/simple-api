05f1b0b5362f8e6b7cbaaa4140b066b84a6b77de
Gideon Nwokpor <gideonjohnson70@gmail.com>
2023-04-17 15:36:58 +0100

    update to api

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+const userData = require("./routes/data.js");
+
+const port = process.env.PORT || 5000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("This is a server built by Gideon Nwokpor");
+});
+
+app.get("/api/data", (req, res) => {
+  res.json(userData);
+});
+
+app.listen(port, (err) => {
+  if (err) throw err;
+  console.log("Server is running on port " + port);
+});
---
+++ routes/data.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: "Gideon",
+  colors: {
+    primary: "#0074D9",
+    secondary: "#FF4136",
+    tertiary: "#2ECC40",
+    watu: "#395074",
+  },
+};
